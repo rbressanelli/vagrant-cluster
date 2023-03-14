964cf1b60ed7812b93e21c0a3b203c61a3b8fd8c
Roberto Bressanelli <rbressanelli@gmail.com>
2023-03-14 13:57:03 -0300

    exercicio basico

--- worker.sh
+++ worker.sh
@@ -1,1 +1,1 @@
-    docker swarm join --token SWMTKN-1-3pj8k0i4tn77bd93a0yxhgh36hxuef5q5oyg1732rztnfy29ll-a94q0ipwgrjs4xikzyb4yb3n5 10.10.10.100:2377
+    docker swarm join --token SWMTKN-1-0sthauh92uegwl02y6fisfq0scud3t4pke4tawpj9btop27ulo-50jf2zepzsamllpmkolata2r2 10.10.10.100:2377
